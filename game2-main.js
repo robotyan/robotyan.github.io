5d7416b1db9ba40168aff81c5ca7109d9f3f6d03
catwinglam <46949241+catwinglam@users.noreply.github.com>
2021-05-31 22:17:01 +0800

    game2

--- game2-main.js
+++ game2-main.js
@@ -1,200 +1,133 @@
-/////////////////////////////game page/////////////////////////////
-// 0 - coverDraw
-// 1 - prefaceDraw
-// 2 - gameDraw
-// 3 - endDraw
-///////////////////////////////////////////////////////////////////
+
+
+
+let prefaceEN;
+let prefaceCHI;
+let selectBG;
+
+let kuchunyin_01;
+let kuchunyin_02;
+let leungkahim_01;
+let leungkahim_02;
+let nghowah_01;
+let nghowah_02;
+let nureni_01;
+let nureni_02;
+let winghei_01;
+let winghei_02;
+let xerxes_01;
+let xerxes_02;
+
+let lang = 0;
 
 ////////page///////////
 let page = 0;
 let pageCount = 0;
 
-//
-// let input;
-// let img1;
+///
 
-//////////////img///////////////////
-let img, bg, hover;
-var vScale = 25;
-
-let cubeBW = [];
-let cubeColor = [];
-let cover;
-let bw;
-
-//game 6 sets//
-
-///////////////////////////saveImg///////////////////////////
-let gameCanvas;
-var snapshot = [];
-let snapCount = 0;
-// let newImg;
+let sliderY = 40;
 
 
 
 function preload(){
-  bw =  loadImage('../assets/WDWT-game2/WDWTgame2-BW-cover.png');
-  cover =  loadImage('../assets/WDWT-game2/WDWTgame2-color-cover.png');
-  for (var i = 1; i < 7; i++) {
-    cubeBW[i] = loadImage('../assets/WDWT-game2/WDWTgame2-BW-0' + i + '.png');
-  }
-  for (var i = 1; i < 7; i++) {
-    cubeColor[i] = loadImage('../assets/WDWT-game2/WDWTgame2-color-0' + i + '.png');
-  }
+  prefaceEN =  loadImage('../assets/WDWT-game2/preface_en.png');
+  prefaceCHI =  loadImage('../assets/WDWT-game2/preface_chi.png');
+  selectBG =  loadImage('../assets/WDWT-game2/select_bg.png');
+  kuchunyin_01 = loadImage('../assets/WDWT-game2/artworks/ku_chun_yin_1.png');
+  kuchunyin_02 = loadImage('../assets/WDWT-game2/artworks/ku_chun_yin_2.png');
+  leungkahim_01 = loadImage('../assets/WDWT-game2/artworks/leung_ka_him_1.png');
+  leungkahim_02 = loadImage('../assets/WDWT-game2/artworks/leung_ka_him_2.png');
+  nghowah_01 = loadImage('../assets/WDWT-game2/artworks/ng_ho_wah_1.png');
+  nghowah_02 = loadImage('../assets/WDWT-game2/artworks/ng_ho_wah_2.png');
+  nureni_01 = loadImage('../assets/WDWT-game2/artworks/Nureni_1.png');
+  nureni_02 = loadImage('../assets/WDWT-game2/artworks/Nureni_2.png');
+  winghei_01 = loadImage('../assets/WDWT-game2/artworks/winghei_1.png');
+  winghei_02 = loadImage('../assets/WDWT-game2/artworks/winghei_2.png');
+  xerxes_01 = loadImage('../assets/WDWT-game2/artworks/xerxes_1.png');
+  xerxes_02 = loadImage('../assets/WDWT-game2/artworks/xerxes_2.png');
 }
 
 function setup() {
-
-  gameCanvas =  createCanvas(500, 500).drop(gotFile).dragOver(highlight).dragLeave(redraw);
-  // colorMode(RGB).imageMode(CORNER);
-
-  // pixelDensity(1);
-  hover = color('yellow');
-
+  gameCanvas =  createCanvas(500, 500);
+  noStroke();
 }
 
 function draw() {
-
  background(255);
-   // if (img1) {
-   //   image(img1, 0, 0, width, height);
-   // }
- switch (page) {
-   case 0:
-     coverDraw();
-     break;
-   case 1:
-     prefaceDraw();
-     break;
-   case 2:
-     gameDraw();
-     break;
-   case 3:
-     endDraw();
-     break;
-   case 4:
-     thankyouDraw();
-     break;
-   default:
-  }
+ // prefaceDraw();
+switch (page) {
+  case 0:
+    prefaceDraw();
+    break;
+  case 1:
+    selectDraw();
+    break;
+  case 2:
+    // layoutDraw();
+    break;
+  case 3:
+    // gameDraw();
+    break;
+  case 4:
+    // thankyouDraw();
+    break;
+  default:
+ }
 
 
 }
-
-function coverDraw(){
-  pageCount++;
-  background(255);
-  image(cover, 0, 0, width, height);
-
-  if (mouseIsPressed && pageCount >10){
-    page = 1;
-    pageCount=0;
-  }
-}
-
 
 function prefaceDraw(){
   pageCount++;
   background(255);
-  image(bw, 0, 0, width, height);
+  // image(prefaceCHI, 0, 0, width, height);
 
-  if (mouseIsPressed && pageCount >10){
-    // saveCanvas(gameCanvas,"screenshot","png");
-    page = 2;
-    pageCount=0;
-  }
-}
-
-function gameDraw(){
-  if (pageCount < 1){
-    textAlign(CENTER).textSize(22).textStyle(BOLD).noLoop();
-    fill(120).noStroke();
-  }
-  pageCount++;
-
-  if (img) {
-    img.resize( vScale-5 ,vScale-5);
-    filter(GRAY);
-    // image(img, 0, 0);
-    img.loadPixels();
-if (pageCount < 3 ){
-    for (var y = 0; y < img.height; y++) {
-      for (var x = 0; x < img.width; x++) {
-
-        var index = (img.width + x + (y * img.width)) * 4;
-        var r = img.pixels[index + 0] ;
-        var g = img.pixels[index + 1] ;
-        var b = img.pixels[index + 2] ;
-        var bright = (r + g + b) / 3 ;
-        // var w = map(bright, 0, 255, 0, vScale);
-        var w = int(map(bright, 0, 255, 6, 1));
-        noStroke();
-        fill(0);
-        rectMode(CENTER);
-        text(pageCount, width>>1, height>>1);
-        snapshot = image(cubeColor[w], x * vScale, y * vScale, vScale, vScale);
-      }
-    }
-  } else if (pageCount ==2 ){
-    if (mouseIsPressed && pageCount >1){
-
-      text('hi', width>>1, height>>1);
-      // saveCanvas(snapshot,"screenshot","png");
-      // pageCount = 0;
-      // page = 3;
-    }
-  }
-  } else {
-    text('Drag an image file onto the canvas.', width>>1, height>>1);
-
-  }
-
-
-
-
-}
-
-function gotFile(f) {
- if (f.type === 'image')  {
-   img = loadImage(f.data, redraw);
-   // img.size(canvas.width / vScale, canvas.height / vScale);
-
- } else   {
-     print(`"${f.name}" isn't an image file!`);}
-}
-
-function highlight(evt) {
- this.background(hover);
- evt.preventDefault();
-}
-
-function endDraw(){
-  pageCount++;
-  background(255);
-  if (mouseIsPressed && pageCount >0){
-      saveCanvas(gameCanvas,"screenshot","png");
+  if (mouseX > 420 && mouseX < 480 && mouseY > 30 && mouseY < 80 && mouseIsPressed && pageCount >10 ){
+    lang = (lang + 1)%2 ;
     pageCount = 0;
-  // image(, 0, 0, 500, 500);
   }
-  // if (mouseX > 500-207 && mouseX < 500 && mouseY > 500-85 && mouseY < 500 && mouseIsPressed && pageCount >10){
-  //     saveCanvas(snapshot,"screenshot","png");
-  //   pageCount = 0;
-  //
-  // }
 
+  if (lang == 0){
+    image(prefaceCHI, 0, 0, 500, 500);
+  } else {
+    image(prefaceEN, 0, 0, 500, 500);
+  }
 
+  if (mouseX > 380 && mouseX < 380+100 && mouseY > 440 && mouseY < 440+40 && mouseIsPressed && pageCount >10){
+    page = 1;
+    pageCount = 0;
+  }
 }
 
 
 
+function selectDraw(){
+  pageCount++;
+image(selectBG, 0, 0, 500, 500);
+fill( '#F3BC4A');
+rect( 450, 30, 14, 120);
+rect (sliderY, 410, 80, 16);
+// sliderMove = map(slider, 10, 410, 0, -6800);
+if (mouseX > sliderY && mouseX < sliderY + 80 && mouseY > 410 && mouseY < 410 + 16 && mouseIsPressed && pageCount >10){
+  if (mouseX < 50){
+    sliderY = 10;
+  } else if (mouseX > 450){
+    sliderY = 410;
+  } else {
+    sliderY = mouseX - 40;
+  }
+}
+
+fill(255);
+rect( 30, 30, 120, 120);
+image(kuchunyin_01, 30, 30, 120, 120);
+rect( 170, 30, 120, 120);
+image(kuchunyin_02, 170, 30, 120, 120);
+rect( 310, 30, 120, 120);
+image(leungkahim_01, 310, 30, 120, 120);
 
 
-// function handleFile(file) {
-//   print(file);
-//   if (file.type === 'image') {
-//     img1 = createImg(file.data, '');
-//     img1.hide();
-//   } else {
-//     img1 = null;
-//   }
-// }
+
+
+}
