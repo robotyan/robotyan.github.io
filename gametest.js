7caa88ab3b4cbc1735e5c94f141f53c4545c8999
cat.alog <46949241+catwinglam@users.noreply.github.com>
2021-06-16 11:29:20 +0800

    up

--- gametest.js
+++ gametest.js
@@ -1,22 +1,18 @@
-//https://p5js.org/examples/structure-create-graphics.html
-var video;
+var saveCanvas;
 
 function setup(){
-  createCanvas(600, 500);
-  video = createCapture(VIDEO);
-  video.size(400, 300);
-  // video.hide();
+  ...
+  saveCanvas =  createGraphics(height, height);
 }
 
 function draw(){
-  fill(0, 12);
-  rect(0, 0, width, height);
-  fill(255);
-  noStroke();
-  image(video,mouseX- 100, mouseY, 60, 60);
+  ...
 }
 
- function mousePressed(){
-   fill(0);
-   rect(0, 0, width, height);
- }
+function keyPressed() {
+  if (key == 's') {
+    let c = get(width/2-height/2,0, height, height);
+    saveCanvas.image(c, 0, 0);
+    save(saveCanvas, frameCount+".png");
+  }
+}
